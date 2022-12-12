edf395ca8cb6157d38892c997d7d7482701aef59
eugeniopuccio <114490589+eugeniopuccio@users.noreply.github.com>
2022-12-12 08:35:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int n;
+    cin>>n;
+    float a[n];
+    float x;
+for(int i=0; i<n; i++){
+    cin>>x;
+    a[i]=x;
+}
+cout<<valmax(a,n);
+    return 0;
+
 }
